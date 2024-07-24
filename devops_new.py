4fa7d5919718a6ac642d05897545a2be863caee7
RohitUpadhye <163296009+RohitUpadhye@users.noreply.github.com>
2024-07-24 21:25:47 +0530

    Update devops_new.py
    to check

--- devops_new.py
+++ devops_new.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1BnMyyFSZeUIYo60D3tJI0MfL20yLTM5k
 """
 
-Animals = ['cat','dog','turtle']
+Animals = ['cat','dog','turtle','lion']
 
 for x in Animals:
   print(Animals)
